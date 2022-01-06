0989a464ade81811ccf6160ef3b6cb549c816407
SuuCH <ntrikrik1027@gmail.com>
2022-01-06 21:55:11 +0900

    fix: クライアント側でも読み取れるようにNEXT_PUBLIC_のついた環境変数に変更

--- lib/api/fetcher.ts
+++ lib/api/fetcher.ts
@@ -1,4 +1,4 @@
 export const fetcher = (url: string) =>
   fetch(url, {
-    headers: { "X-API-KEY": process.env.X_API_KEY } as HeadersInit,
+    headers: { "X-API-KEY": process.env.NEXT_PUBLIC_X_API_KEY } as HeadersInit,
   }).then((res) => res.json());
