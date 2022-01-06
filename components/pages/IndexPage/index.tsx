f833b51bcd7978602c5753ae4ea8c0862b4c2c56
SuuCH <ntrikrik1027@gmail.com>
2022-01-06 23:49:08 +0900

    feat: ページヘッダー、ページフッター、HtmlHeadの配置

--- components/pages/IndexPage/index.tsx
+++ components/pages/IndexPage/index.tsx
@@ -2,6 +2,9 @@ import type { CSSProperties, VFC } from "react";
 
 import { CheckBoxContainer } from "../../organisms/CheckBoxContainer";
 import { GraphContainer } from "../../organisms/GraphContainer";
+import { Footer } from "../Footer";
+import { Header } from "../Header";
+import { HtmlHead } from "../HtmlHead";
 
 const Styles: { [key: string]: CSSProperties } = {
   container: {},
@@ -10,10 +13,13 @@ const Styles: { [key: string]: CSSProperties } = {
 const IndexPage: VFC = () => {
   return (
     <>
+      <HtmlHead title="PopulationTransition" />
+      <Header />
       <main style={Styles.container}>
         <CheckBoxContainer />
         <GraphContainer />
       </main>
+      <Footer />
     </>
   );
 };
